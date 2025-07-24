1989722aa80b135a496d402fcb297ad6d2e34cf9
Martin Slabber <martin@tsolo.io>
2025-07-24 07:34:05 +0200

    fix: Adjust the ATX PSU mounting holes.

--- src/cycax_parts/powersupplies/pc.py
+++ src/cycax_parts/powersupplies/pc.py
@@ -1,49 +1,38 @@
 # SPDX-FileCopyrightText: 2025 Tsolo.io
 #
 # SPDX-License-Identifier: Apache-2.0
+"""Generic PC Power Supplies."""
 
 from cycax.cycad import Print3D
 
 
-class SilverstonetekFlexATX(Print3D):
+class ATX(Print3D):
+    """See ATXPS2-3.jpg.
+
+    Note: This only defines the PS2 ATX PSU and not the PS3 model, the PS3 has the same mounting holes but is shorter.
+    """
+
     def __init__(self):
-        super().__init__(part_no="psu-flexatx-silverstonetek", x_size=81.5, y_size=150, z_size=40.50)
+        super().__init__(part_no="psu-atx-generic", x_size=150.0, y_size=140.0, z_size=64.0)
         self.colour = "black"
         # There is a 2mm overhang at the back and left for the connector plate, if that is builtin.
 
     def definition(self):
         # Top is the Sticker.
         # Back is where the C14 AC connector is, the back of the server.
-        x = 4.4  # Assume the hole placement is the same from both sides.
-        y = (self.z_size - 32) / 2  # Holes are 32mm apart
-        self.back.hole(pos=(self.x_size - x, y), diameter=4.2, depth=4.0)
-        self.back.hole(pos=(self.x_size - x, y), diameter=3.2, external_subtract=True)
-        self.back.hole(pos=(self.x_size - x, y + 32), diameter=4.2, depth=4.0)
-        self.back.hole(pos=(self.x_size - x, y + 32), diameter=3.2, external_subtract=True)
-        self.back.hole(pos=(x, 36), diameter=4.2, depth=4.0)
-        self.back.hole(pos=(x, 36), diameter=3.2, external_subtract=True)
-        self.back.hole(pos=(15.2, 37), diameter=4.2, depth=4.0)
-        self.back.hole(pos=(15.2, 37), diameter=3.2, external_subtract=True)
+        from_edge = (self.x_size - 138) / 2
+        self.back.hole(pos=(from_edge, self.y_size - from_edge), diameter=4.2, depth=4.0)
+        self.back.hole(pos=(from_edge, self.y_size - from_edge), diameter=3.2, external_subtract=True)
+        self.back.hole(pos=(from_edge + 138, self.y_size - from_edge), diameter=4.2, depth=4.0)
+        self.back.hole(pos=(from_edge + 138, self.y_size - from_edge), diameter=3.2, external_subtract=True)
+        self.back.hole(pos=(from_edge, self.y_size - from_edge - 64), diameter=4.2, depth=4.0)
+        self.back.hole(pos=(from_edge, self.y_size - from_edge - 64), diameter=3.2, external_subtract=True)
+        self.back.hole(pos=(from_edge + 114, from_edge), diameter=4.2, depth=4.0)
+        self.back.hole(pos=(from_edge + 114, from_edge), diameter=3.2, external_subtract=True)
+
         # TODO: Define box for C14 power ports.
         # self.back.box(pos=(2, 2), length=24, width=32, external_subtract=True)
 
         # TODO: Define Airvent holes.
         # TODO: Define construction box for internal fan.
         # TODO: Define construction box for cables out of PSU.
-
-        # The following holes void the warranty when used.
-        # Since we have to remove the screws holding the PSU together.
-        # In some applications that was the only option for mounting the PSU.
-        from_bottom = 8.2 + 2.6  # Measured
-        from_front_0 = 6.0 + 2.6  # Measured
-        from_front_r = 128.6 + 2.6  # Measured
-        from_front_l = 120 + 2.6  # Measured
-        hole_pos = (
-            (self.left, self.y_size - from_front_0),
-            (self.left, self.y_size - from_front_l),
-            (self.right, from_front_0),
-            (self.right, from_front_r),
-        )
-        for side, x in hole_pos:
-            side.hole(pos=(x, from_bottom), diameter=3.2, external_subtract=True)
-            side.hole(pos=(x, from_bottom), diameter=3.0, depth=4)
