6fe25f3cf0cac823fe90fc86c3790a5b57ea7987
Khadija <khadija@tsolo.io>
2025-07-30 11:56:00 +0200

    Adjusted ATX hole placements. Added cutout to back for power socket etc.

--- src/cycax_parts/powersupplies/pc.py
+++ src/cycax_parts/powersupplies/pc.py
@@ -13,7 +13,7 @@ class ATX(Print3D):
     """
 
     def __init__(self):
-        super().__init__(part_no="psu-atx-generic", x_size=150.0, y_size=140.0, z_size=64.0)
+        super().__init__(part_no="psu-atx-generic", x_size=150.0, y_size=140.0, z_size=86.0)
         self.colour = "black"
         # There is a 2mm overhang at the back and left for the connector plate, if that is builtin.
 
@@ -21,17 +21,17 @@ class ATX(Print3D):
         # Top is the Sticker.
         # Back is where the C14 AC connector is, the back of the server.
         from_edge = (self.x_size - 138) / 2
-        self.back.hole(pos=(from_edge, self.y_size - from_edge), diameter=4.2, depth=4.0)
-        self.back.hole(pos=(from_edge, self.y_size - from_edge), diameter=3.2, external_subtract=True)
-        self.back.hole(pos=(from_edge + 138, self.y_size - from_edge), diameter=4.2, depth=4.0)
-        self.back.hole(pos=(from_edge + 138, self.y_size - from_edge), diameter=3.2, external_subtract=True)
-        self.back.hole(pos=(from_edge, self.y_size - from_edge - 64), diameter=4.2, depth=4.0)
-        self.back.hole(pos=(from_edge, self.y_size - from_edge - 64), diameter=3.2, external_subtract=True)
-        self.back.hole(pos=(from_edge + 114, from_edge), diameter=4.2, depth=4.0)
+        self.back.hole(pos=(from_edge, self.z_size - from_edge), diameter=3, depth=4.0)
+        self.back.hole(pos=(from_edge, self.z_size - from_edge), diameter=3.2, external_subtract=True)
+        self.back.hole(pos=(from_edge + 138, self.z_size - from_edge), diameter=3, depth=4.0)
+        self.back.hole(pos=(from_edge + 138, self.z_size - from_edge), diameter=3.2, external_subtract=True)
+        self.back.hole(pos=(from_edge, self.z_size - from_edge - 64), diameter=3, depth=4.0)
+        self.back.hole(pos=(from_edge, self.z_size - from_edge - 64), diameter=3.2, external_subtract=True)
+        self.back.hole(pos=(from_edge + 114, from_edge), diameter=3, depth=4.0)
         self.back.hole(pos=(from_edge + 114, from_edge), diameter=3.2, external_subtract=True)
 
-        # TODO: Define box for C14 power ports.
-        # self.back.box(pos=(2, 2), length=24, width=32, external_subtract=True)
+        # Define large box for C14 power ports, on/off switch etc.
+        self.back.box(pos=(10, 10), length=self.x_size - 20, width=self.z_size - 20, external_subtract=True)
 
         # TODO: Define Airvent holes.
         # TODO: Define construction box for internal fan.
